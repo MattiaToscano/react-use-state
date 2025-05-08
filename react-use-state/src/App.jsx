33c0e96becbb938dab819fd493abf5ef455fdfc8
MattiaToscano <mattia1997ct@gmail.com>
2025-05-08 13:35:17 +0200

    [UPD] Stilizzati bottoni e h1

--- react-use-state/src/App.jsx
+++ react-use-state/src/App.jsx
@@ -1,24 +1,27 @@
 import { useState } from 'react'
 import languages from './languages'
 
-function App() {
-  return (
-    <>
-      <div>
-        <h1>Learn Web development</h1>
-      </div>
-      <div className="container">
-        <ul>
-          {languages.map((language) => (
-            <li key={language.id}>
-              <button>{language.title}</button>
-            </li>
-          ))}
-        </ul>
-      </div>
+const App = () => {
+  const [selectedLanguage, setSelectedLanguage] = useState(null)
 
-    </>
-  )
+}
+return (
+  <>
+    <div>
+      <h1>Learn Web development</h1>
+    </div>
+    <div className="container">
+      <ul>
+        {languages.map((language) => (
+          <li key={language.id}>
+            <button className="btn">{language.title}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+  </>
+)
 }
 
 export default App
