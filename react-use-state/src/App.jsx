e97f288565e3ac1f8810bc4ef48d8de8a25074b1
MattiaToscano <mattia1997ct@gmail.com>
2025-05-08 13:48:04 +0200

    [UPD] Inserito l'evento click

--- react-use-state/src/App.jsx
+++ react-use-state/src/App.jsx
@@ -1,27 +1,37 @@
 import { useState } from 'react'
 import languages from './languages'
+import React from 'react'
 
 const App = () => {
-  const [selectedLanguage, setSelectedLanguage] = useState(null)
+  const [activeLanguage, setActiveLanguage] = useState(null)
+  console.log(activeLanguage)
 
-}
-return (
-  <>
-    <div>
-      <h1>Learn Web development</h1>
-    </div>
-    <div className="container">
-      <ul>
-        {languages.map((language) => (
-          <li key={language.id}>
-            <button className="btn">{language.title}</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-
-  </>
-)
+  return (
+    <>
+      <div>
+        <h1>Learn Web development</h1>
+      </div>
+      <div className="container">
+        <div className="card">
+          <div className="card-item">
+            <ul>
+              {languages.map((language) => (
+                <li key={language.id}>
+                  <button
+                    className={`btn ${language.title.toLowerCase()}`}
+                    onClick={() => setActiveLanguage(language)}
+                  >
+                    {language.title}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <p>Ciao ciao</p>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App
